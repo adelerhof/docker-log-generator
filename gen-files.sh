4c6e8c83b6a545563de7c77b1aaf2ae2d6bb38ff
Erik Adelerhof <erik@adelerhof.eu>
2022-06-28 11:18:08 +0200

    fix date notation

--- gen-files.sh
+++ gen-files.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DATE=$(date +"%d-%m-%Y")
+DATE=$(date +"%d-%m-%Y_%H%M%S")
 SOURCE_LOGS=/home/avr/logs
 cd ${SOURCE_LOGS}
 for i in {1..20}; do dd if=/dev/urandom bs=1M count=1 of=file$DATE$i; done
